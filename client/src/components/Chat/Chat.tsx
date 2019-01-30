a0179ade0f2313597451c963739d2c8c15368e33
liamross <liamross.code@gmail.com>
2019-01-30 12:13:18 -0800

    Updated chat component to use editable text

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -1,24 +1,27 @@
-import React, {useState, useRef} from 'react';
+import React, {useState} from 'react';
 import ChatInput from '../ChatInput/ChatInput';
 import s from './Chat.module.scss';
 
 interface IChatProps {}
 
 const Chat: React.SFC<IChatProps> = () => {
-  const inputEl = useRef<HTMLDivElement>(null);
   const [message, setMessage] = useState('');
 
   const handleSubmit = () => {
     // TODO: Something with message.
-    const chatRef = inputEl.current;
-    if (chatRef) chatRef.innerText = '';
+    console.log('Submit: ', message);
+    setMessage('');
   };
 
   return (
     <div className={s.component}>
       <div className={s.messages}>{''}</div>
       <div className={s.input}>
-        <ChatInput ref={inputEl} onType={setMessage} onSubmit={handleSubmit} />
+        <ChatInput
+          value={message}
+          onChange={setMessage}
+          onSubmit={handleSubmit}
+        />
       </div>
     </div>
   );
--- client/src/components/ChatInput/ChatInput.tsx
+++ client/src/components/ChatInput/ChatInput.tsx
@@ -1,43 +1,37 @@
-import {Button} from '@blueprintjs/core';
+import {Button, EditableText} from '@blueprintjs/core';
 import React, {memo} from 'react';
-import useEventListener from '../../hooks/useEventListener';
 import s from './ChatInput.module.scss';
 
 interface IChatInputProps {
-  onType: (newValue: string) => any;
+  value: string;
+  onChange: (newValue: string) => any;
   onSubmit?: () => any;
 }
 
-const CHAT_ID = 'chat-input-id';
-
-const ChatInput = React.forwardRef<HTMLDivElement, IChatInputProps>(
-  ({onType, onSubmit}, ref) => {
-    // Listen for inputs, call onType with text content.
-    const onChangeListener = () => {
-      const chatElement = document.getElementById(CHAT_ID);
-      if (chatElement) onType(chatElement.innerText || '');
-    };
-    useEventListener(CHAT_ID, 'input', onChangeListener);
-
-    // Listen for `Enter` keypress without shift to fire onSubmit.
-    const onKeyPressListener = (event: KeyboardEvent) => {
-      if (onSubmit && event.keyCode === 13 && !event.shiftKey) {
-        event.preventDefault();
-        // event.stopImmediatePropagation();
-        onSubmit();
-      }
-    };
-    useEventListener(CHAT_ID, 'keypress', onKeyPressListener);
-
-    return (
-      <>
-        <div className={s.component}>
-          <div ref={ref} contentEditable id={CHAT_ID} className={s.input} />
-          <Button onClick={onSubmit} minimal icon="arrow-right" />
-        </div>
-      </>
-    );
-  },
-);
+const ChatInput: React.SFC<IChatInputProps> = ({value, onChange, onSubmit}) => {
+  return (
+    <>
+      <div className={s.component}>
+        <EditableText
+          className={s.input}
+          maxLines={4}
+          minLines={1}
+          multiline={true}
+          placeholder="Send message"
+          confirmOnEnterKey
+          value={value}
+          onChange={onChange}
+          // onConfirm={onSubmit} // Need to just detect enter message
+        />
+        <Button
+          className={s.button}
+          onClick={onSubmit}
+          minimal
+          icon="arrow-right"
+        />
+      </div>
+    </>
+  );
+};
 
 export default memo(ChatInput);
