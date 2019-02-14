6202acecef1a27b9fd09a7f3bf50c077ed5ff287
liamross <liamross.code@gmail.com>
2019-02-14 20:20:46 +0200

    Updated apollo client config

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,8 @@
 import {InMemoryCache} from 'apollo-cache-inmemory';
 import {ApolloClient} from 'apollo-client';
+import {ApolloLink} from 'apollo-link';
 import {setContext} from 'apollo-link-context';
+import {onError} from 'apollo-link-error';
 import {HttpLink} from 'apollo-link-http';
 import React from 'react';
 import {ApolloProvider} from 'react-apollo';
@@ -15,10 +17,9 @@ import * as serviceWorker from './serviceWorker';
 const cache = new InMemoryCache();
 
 // Connect to the API.
-const link = new HttpLink({
-  uri: 'http://localhost:4000/graphql',
-});
+const link = new HttpLink({uri: 'http://localhost:4000/graphql'});
 
+// Add authentication to the HTTP link.
 const authLink = setContext((_, {headers}) => {
   // Get the authentication token from local storage if it exists.
   const token = localStorage.getItem('token');
@@ -34,7 +35,23 @@ const authLink = setContext((_, {headers}) => {
 // Build Apollo client.
 const client = new ApolloClient({
   cache,
-  link: authLink.concat(link),
+  link: ApolloLink.from([
+    onError(({graphQLErrors, networkError}) => {
+      if (graphQLErrors) {
+        graphQLErrors.map(({message, locations, path}) => {
+          // tslint:disable-next-line:no-console
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          );
+        });
+      }
+      if (networkError) {
+        // tslint:disable-next-line:no-console
+        console.log(`[Network error]: ${networkError}`);
+      }
+    }),
+    authLink.concat(link),
+  ]),
   initializers, // Alpha
   resolvers, // Alpha
   typeDefs, // Alpha
